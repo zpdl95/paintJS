2663062b062822002b9640bb875cbc85d51d970d
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-16 20:13:02 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
 
 let painting = false;
 
 function onMouseMove(e) {
   const { offsetX: x } = e;
   const { offsetY: y } = e;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.stroke();
+  }
 }
 
-function onMouseDown() {
-  painting = true;
-}
-
-function onMouseUp() {
+function stopPainting() {
   painting = false;
+}
+
+function startPainting() {
+  painting = true;
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
 }
 
 /* clientX or clientY = 윈도우에서 마우스 위치 */
