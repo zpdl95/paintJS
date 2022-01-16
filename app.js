f07a3118b054c367502af8186550910f390b02ce
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-17 01:35:45 +0900

    #2.5 Filling Mode

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const colors = document.querySelectorAll(".jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
 const state = document.getElementById("jsState");
+const allClear = document.getElementById("jsAllClear");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -14,16 +15,18 @@ let filling = false;
 function onMouseMove(e) {
   const { offsetX: x } = e;
   const { offsetY: y } = e;
-  if (!painting) {
-    /* beginPath() = 경로생성 */
-    ctx.beginPath();
-    /* moveTo() = 시작지점생성 */
-    ctx.moveTo(x, y);
-  } else {
-    /* lineTo() = 이동지점생성 */
-    ctx.lineTo(x, y);
-    /* stroke() = 그리기 */
-    ctx.stroke();
+  if (!filling) {
+    if (!painting) {
+      /* beginPath() = 경로생성 */
+      ctx.beginPath();
+      /* moveTo() = 시작지점생성 */
+      ctx.moveTo(x, y);
+    } else {
+      /* lineTo() = 이동지점생성 */
+      ctx.lineTo(x, y);
+      /* stroke() = 그리기 */
+      ctx.stroke();
+    }
   }
 }
 
@@ -50,13 +53,14 @@ function handleColorClick(e) {
     },
   } = e;
   ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRangeChange(e) {
   ctx.lineWidth = e.target.value;
 }
 
-function handleModeClick(e) {
+function handleModeClick() {
   if (filling) {
     filling = false;
     mode.innerText = "fill";
@@ -68,22 +72,31 @@ function handleModeClick(e) {
   }
 }
 
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function handleAllClear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   window.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseenter", onMouseEnter);
+  canvas.addEventListener("click", handleCanvasClick);
 }
 
 colors.forEach((color) => color.addEventListener("click", handleColorClick));
 
-if (range) {
-  range.addEventListener("input", handleRangeChange);
-}
+range?.addEventListener("input", handleRangeChange);
 
-if (mode) {
-  mode.addEventListener("click", handleModeClick);
-}
+mode?.addEventListener("click", handleModeClick);
+
+allClear?.addEventListener("click", handleAllClear);
 
 /* clientX or clientY = 윈도우에서 마우스 위치 */
 /* offsetX or offsetY = 해당태크에서 마우스 위치 */
