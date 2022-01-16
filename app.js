b4024a0b29b7d244437a7077c00e340e86d4ca0a
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-16 23:34:57 +0900

    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.querySelectorAll("#jsColor");
+
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
 
 let painting = false;
 
@@ -7,12 +11,23 @@ function onMouseMove(e) {
   const { offsetX: x } = e;
   const { offsetY: y } = e;
   if (!painting) {
+    /* beginPath() = 경로생성 */
     ctx.beginPath();
+    /* moveTo() = 시작지점생성 */
     ctx.moveTo(x, y);
   } else {
+    /* lineTo() = 이동지점생성 */
     ctx.lineTo(x, y);
-    ctx.closePath();
+    /* stroke() = 그리기 */
     ctx.stroke();
+  }
+}
+
+function onMouseEnter(e) {
+  const { offsetX: x } = e;
+  const { offsetY: y } = e;
+  if (painting) {
+    ctx.moveTo(x, y);
   }
 }
 
@@ -24,11 +39,23 @@ function startPainting() {
   painting = true;
 }
 
+function handleColorClick(e) {
+  const {
+    target: {
+      style: { backgroundColor: color },
+    },
+  } = e;
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting);
+  window.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseenter", onMouseEnter);
 }
+
+colors.forEach((color) => color.addEventListener("click", handleColorClick));
 
 /* clientX or clientY = 윈도우에서 마우스 위치 */
 /* offsetX or offsetY = 해당태크에서 마우스 위치 */
