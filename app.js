1b5f477928dbc2a427f38deb3dd3328c78e556b3
zpdl95 <59528534+zpdl95@users.noreply.github.com>
2022-01-17 01:07:26 +0900

    #2.4 Brush Size

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
-const colors = document.querySelectorAll("#jsColor");
+const colors = document.querySelectorAll(".jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const state = document.getElementById("jsState");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
 let painting = false;
+let filling = false;
 
 function onMouseMove(e) {
   const { offsetX: x } = e;
@@ -48,6 +52,22 @@ function handleColorClick(e) {
   ctx.strokeStyle = color;
 }
 
+function handleRangeChange(e) {
+  ctx.lineWidth = e.target.value;
+}
+
+function handleModeClick(e) {
+  if (filling) {
+    filling = false;
+    mode.innerText = "fill";
+    state.innerText = "painting...";
+  } else {
+    filling = true;
+    mode.innerText = "paint";
+    state.innerText = "filling...";
+  }
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
@@ -56,6 +76,14 @@ if (canvas) {
 }
 
 colors.forEach((color) => color.addEventListener("click", handleColorClick));
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
 
 /* clientX or clientY = 윈도우에서 마우스 위치 */
 /* offsetX or offsetY = 해당태크에서 마우스 위치 */
